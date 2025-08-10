66af2c3bfa50a98f3e7bf88c55e8374376cccfbc
client <client@Windows-11.ToreaTravelCHC.local>
2025-08-10 17:43:32 +1200

    adding the imports to setup the python tool, alongside integrating the YARA path, rules file, VT details (api key, url, upload url) etc. My next step is to start adding the GUI, for which I will then add backend logic to match

---
+++ tredr.py
@@ -0,0 +1,16 @@
+import subprocess # lets me run external programs
+import os # to interact with the operating system
+import hashlib # to have an interface to hash files
+import requests # to interact with web services/make HTTP requests
+import threading # to use concurrency for running tasks
+import time # getting current date and time
+import tkinter as tk # for the GUI
+from tkinter import filedialog, scrolledtext, ttk
+from datetime import datetime
+
+YARA_PATH = "yara64.exe"
+RULES_FILE = "rules\\trojan_rules.yar"
+VT_API_KEY = "34923601df873108e50af7f497e636c88f6087851ca5321dde99cfebec76f509"
+VT_URL = "https://virustotal.com/api/v3/files/"
+UPLOAD_URL = "https://www.virustotal.com/api/v3"
+DELAY = 15 # api rate limit is 1 req per 15 sec
