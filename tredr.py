6e24cac856b67258257169ec71891e7dd339e1d8
client <client@Windows-11.ToreaTravelCHC.local>
2025-08-11 11:56:13 +1200

    Added a quick screen for the GUI showing the scan button and results - was able to test it at one point but added code which is not available yet (havent added backend logic yet) but will be added once GUI is completed. This design is very basic right now and will make it look more interesting - will likely add a 'tab' section called a notebook (as said by tkinter), which will allow me to have multiple pages

--- tredr.py
+++ tredr.py
@@ -14,3 +14,33 @@ VT_API_KEY = "34923601df873108e50af7f497e636c88f6087851ca5321dde99cfebec76f509"
 VT_URL = "https://virustotal.com/api/v3/files/"
 UPLOAD_URL = "https://www.virustotal.com/api/v3"
 DELAY = 15 # api rate limit is 1 req per 15 sec
+
+# -------- GUI --------
+root = tk.Tk() # main window
+root.title("TREDR - Trojan Risk Education & Detection Resource") # main title seen in the window 
+root.geometry("760x500") # size of the entire screen
+
+title_label = tk.Label(root, text="YARA analysis + VirusTotal hybrid verification", 
+                       font=("Helvetica", 18, "bold"), bg="#f0f2f5", fg="#1a1a1a")
+title_label.pack(pady=10)
+
+desc_label = tk.Label(root, text="Choose a folder to scan for suspicious files using YARA + VirusTotal", font=("Helvetica", 10), bg="#f0f2f5")
+desc_label.pack(pady=5)
+
+frame = tk.Frame(root)
+frame.pack(pady=10)
+
+def browse_and_scan():
+    folder = filedialog.askdirectory()
+    if folder:
+        scan_folder(folder) # not applicable until backend logic is set
+    if not folder:
+        return
+    
+scan_button = tk.Button(frame, text="Select Folder & Scan", command=browse_and_scan)
+scan_button.pack()
+
+output_box = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=85, height=25)
+output_box.pack(padx=10, pady=10)
+output_box.configure(state="disabled") # readonly
+
