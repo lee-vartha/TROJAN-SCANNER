0608797d403553490b0f0b4325c66e510d5eb691
Lee Vartha <lee-vartha@live.nmit.ac.nz>
2025-08-12 11:32:25 +1200

    got a very basic setup working for the backend logic design - the output box information is quite plain and hard to read so might make colour tags to make it more interesting to look at and make it easier to map out different responses. I would also make the structure nicer to look at too - make things more user-friendly. will need to do better with the history tab too. for all these next-steps, i  will do all this ater my supervisor meeting (at least I have something to work with)

--- tredr.py
+++ tredr.py
@@ -15,6 +15,100 @@ VT_URL = "https://virustotal.com/api/v3/files/"
 UPLOAD_URL = "https://www.virustotal.com/api/v3"
 DELAY = 15 # api rate limit is 1 req per 15 sec
 
+def scan_folder(folder_path, output_box):
+    output_box.configure(state="normal")
+    output_box.delete(1.0, tk.END)
+    scan_button.config(state="disabled")
+    output_box.insert(tk.END, "[+] Running YARA scan...\n")
+
+    yara_result = subprocess.run(
+        [YARA_PATH, RULES_FILE, folder_path],
+        capture_output=True,
+        text=True
+    )
+
+    # yara_result.stdout represents the standard output
+    # .strip() removes whitespace and 
+    # .splitlines() splits output into list
+    matches = yara_result.stdout.strip().splitlines()
+
+    # if theres no match then the output box will say nothings wrong
+    if not matches or matches == ['']:
+        output_box.insert(tk.END, "[-] No suspicious files detected,\n")
+        done_scanning()
+        return
+
+    # otherwise the box will insert
+    output_box.insert(tk.END, "[+] YARA matches found: \n")
+    # creates an empty set, which will list out unordered items
+    suspicious_files = set()
+    # for each line with the match
+    for line in matches:
+        #inserting "=> {line}" into the 'output_box' entry box
+        # tk.END is showing the position on where the text should go - this goes at the end of the context of this string
+        output_box.insert(tk.END, f"=> {line}\n")
+        parts = line.split()
+        # if the length of everything is more than 2 then add the second element
+        if len(parts) >= 2:
+            suspicious_files.add(parts[1])
+
+    # in the same entry box, add 'Checking VirusTotal'
+    output_box.insert(tk.END, "\n[+] Checking VirusTotal... \n")
+
+    # working with the API and inserting the headers
+    headers = {"x-apikey": VT_API_KEY}
+
+
+    for file_path in suspicious_files:
+        output_box.insert(tk.END, f"\n => {file_path}\n")
+        try:
+            # calculating the SHA256 hash 
+            # open the file and read in binary mode
+            with open(file_path, "rb") as f:
+                file_bytes = f.read()
+                sha256 = hashlib.sha256(file_bytes).hexdigest()
+            response = requests.get(VT_URL + sha256, headers=headers)
+
+            # if its successful (200)
+            if response.status_code == 200:
+                data = response.json()
+                stats = data["data"]["attributes"]["last_analysis_stats"]
+                output_box.insert(tk.END, f"VT Detection: {stats['malicious']} malicious, {stats['suspicious']} suspicious\n")
+
+            # if it failed (client error - 400)
+            elif response.status_code == 404:
+                output_box.insert(tk.END, "Not found in VT database. Uploading file...\n")
+
+                files = {"file": (os.path.basename(file_path), open(file_path, "rb"))}
+                upload_response = requests.post(UPLOAD_URL, headers=headers, files=files)
+
+                if upload_response.status_code == 200:
+                    analysis_id = upload_response.json()["data"]["id"]
+                    output_box.insert(tk.END, f"Uploaded. Analysis ID: {analysis_id}\n Waiting for analysis ({DELAY} secs)...\n")
+                    time.sleep(DELAY)
+
+                    result = requests.get(f"https://www.virustotal.com/api/v3/analyses/{analysis_id}", headers=headers)
+                    if result.status_code == 200:
+                        stats = result.json()["data"]["attributes"]["stats"]
+                        output_box.insert(tk.END, f"VT Detection: {stats['malicious']} malicious, {stats['suspicious']} suspicious \n")
+                    else:
+                        output_box.insert(tk.END, f"Couldnt retrieve results: {result.status_code}\n")
+                else:
+                    output_box.insert(tk.END, f"Upload failed: {upload_response.status_code}\n")
+            else:
+                output_box.insert(tk.END, f"VT error: {response.status_code}\n")
+
+        except Exception as e:
+            output_box.insert(tk.END, f"Error: {e}\n")
+
+    done_scanning()
+
+def done_scanning():
+    scan_button.config(state="normal")
+    output_box.insert(tk.END, "\n Scan Complete.")
+    output_box.configure(state="disabled")
+
+
 # -------- GUI --------
 root = tk.Tk() # main window
 root.title("TREDR - Trojan Risk Education & Detection Resource") # main title seen in the window 
@@ -68,7 +162,7 @@ controls.pack(fill="x", padx=12, pady=(0, 8)) # packs
 def browse_and_scan():
     folder = filedialog.askdirectory()
     if folder:
-        scan_folder(folder) # not applicable until backend logic is set
+        scan_folder(folder, output_box) # not applicable until backend logic is set
     if not folder:
         return
     
